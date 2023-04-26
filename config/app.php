f410fb59818317b4bd29de27056440a47cc0a7b2
AbdiwakB <abdiwakbek3226@gmail.com>
2023-04-26 16:53:51 +0300

    Spatie-Permission Installed

--- config/app.php
+++ config/app.php
@@ -194,6 +194,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        // From Spatie
+        Spatie\Permission\PermissionServiceProvider::class, 
 
     ],
 
