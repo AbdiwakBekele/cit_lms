d067e97fd95f4f0e55de5e8ec8f66ca94fad44fe
AbdiwakBekele <66849505+AbdiwakBekele@users.noreply.github.com>
2023-05-23 15:48:09 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HOST="ftp.californiatraininginstitute.com" # Replace with your cPanel FTP server hostname
+USERNAME="cti@californiatraininginstitute.com" # Replace with your cPanel FTP username
+PASSWORD="@FTP.cti" # Replace with your cPanel FTP password
+
+TARGETDIR="/lms.californiatraininginstitute.com" # Replace with the target directory on your cPanel account
+
+echo "Deploying code to cPanel..."
+
+lftp -c "open -u $USERNAME,$PASSWORD $HOST; set ssl:verify-certificate no; mirror -R ./ $TARGETDIR --ignore-time --parallel=10 --exclude-glob .git* --exclude .github* --exclude deploy.sh"
+
+echo "Deployment completed!"
