1b184a06cd607a70156cc160b4f567275e248a42
AbdiwakBekele <66849505+AbdiwakBekele@users.noreply.github.com>
2023-06-07 19:35:18 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ echo "Deploying code to cPanel..."
 
 lftp -c "open -u $USERNAME,$PASSWORD $HOST; set ssl:verify-certificate no; mirror -R ./ $TARGETDIR --ignore-time --parallel=10 --exclude-glob .git* --exclude .github* --exclude deploy.sh"
 
-echo "Deployment completed!"
+echo "Deployment completed Successfully!"
