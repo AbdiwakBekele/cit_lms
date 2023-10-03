3c029c3580dc348981cf27a6290662ede71dbd02
AbdiwakB <abdiwakbek3226@gmail.com>
2023-10-03 17:35:52 +0300

    Ethio Tele email updated

--- resources/views/emails/studentEnrollMail.blade.php
+++ resources/views/emails/studentEnrollMail.blade.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to California Training</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome to California Training</title>
+    </head>
 
-<body>
-    <h1>Congratulation</h1>
+    <body>
+        <h1>Congratulation</h1>
 
-    <p>Dear {{$student_name}},</p>
+        <p>Dear {{$student_name}},</p>
 
-    <p>You successfuly enrolled for the course <strong>{{$course_name}}</strong> on the <strong>{{$batch_name}}</strong>
-        shift. We are excited to have you
-        on board!
-    </p>
-
-    <p>Find the course in <strong>"My Learning"</strong> section. Please feel free to explore our platform and take
-        advantage of our
-        training resources.</p>
-
-    <p>If you have any questions or need assistance, don't hesitate to reach out to our support team at
-        support@edu-cti.com.</p>
+        <p>You successfuly enrolled for the course <strong>{{$course_name}} (Tailored). We are excited to have you
+                on board!
+        </p>
 
 
-    <p>Best regards,</p>
-    <p>The California Training Team</p>
-</body>
+        <p>Find the course in <strong>"My Learning"</strong> section. Please feel free to explore our platform and take
+            advantage of our
+            training resources.</p>
+
+        <p>If you have any questions or need assistance, don't hesitate to reach out to our support team at
+            support@edu-cti.com.</p>
+
+
+        <p>Best regards,</p>
+        <p>The California Training Team</p>
+    </body>
 
 </html>
