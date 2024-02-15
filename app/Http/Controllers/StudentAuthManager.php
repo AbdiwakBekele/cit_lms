b5b2717182a0256d68ce082a1c503c5b3dd98da5
AbdiwakB <abdiwakbek3226@gmail.com>
2024-02-15 15:44:46 +0300

    login api

--- app/Http/Controllers/StudentAuthManager.php
+++ app/Http/Controllers/StudentAuthManager.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Hash;
@@ -14,44 +15,66 @@ use App\Mail\StudentForgetPassword;
 use Illuminate\Support\Str;
 
 
-class StudentAuthManager extends Controller{
-    
-    function login(){
-        if(Auth::guard('student')->check()){
+class StudentAuthManager extends Controller {
+
+    function login() {
+        if (Auth::guard('student')->check()) {
             return redirect()->intended();
         }
         return view('user_student.student_login');
-
     }
 
-    function loginPost(Request $request){
+    function loginPost(Request $request) {
         $request->validate([
-            'email'=>'required',
-            'password'=>'required'
+            'email' => 'required',
+            'password' => 'required'
         ]);
 
         $credential = $request->only('email', 'password');
-        if(Auth::guard('student')->attempt($credential)){
+        if (Auth::guard('student')->attempt($credential)) {
             Session::put('student_session_key', true);
             return redirect()->intended();
-        }else{
+        } else {
             return redirect('/student_login')->with('error', 'Login details are not valid');
         }
     }
 
-    function register(){
+    function register() {
         return view('user_student.student_signup');
     }
 
-    function registrationPost(Request $request){
+    function loginPostApi(Request $request) {
         $request->validate([
-            'fullname'=>'required',
-            'email'=> 'required|email|unique:students',
-            'age'=>'required',
-            'phone'=>'required',
-            'gender'=>'required',
-            'address'=>'required',
-            'password'=>'required'
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::guard('student')->attempt($credentials)) {
+            $student = Auth::guard('student')->user();
+            // $token = $student->createToken('YourAppToken')->accessToken;
+            return response()->json([
+                'success' => true,
+                // 'token' => $token,
+                'user' => $student // Include the user data in the response
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Unauthorized'
+            ], 401);
+        }
+    }
+
+    function registrationPost(Request $request) {
+        $request->validate([
+            'fullname' => 'required',
+            'email' => 'required|email|unique:students',
+            'age' => 'required',
+            'phone' => 'required',
+            'gender' => 'required',
+            'address' => 'required',
+            'password' => 'required'
         ]);
 
         $data['fullname'] = $request->fullname;
@@ -66,42 +89,39 @@ class StudentAuthManager extends Controller{
 
         //Mail::to($user->email)->send(new StudentRegistered($user));
 
-        if(!$user){
+        if (!$user) {
             return redirect('/student_register')->with('error', 'Failed to register');
         }
         return redirect('/student_register')->with('success', 'Registration Successful, login to access the app. ** Please check your email to verify your account **');
     }
 
-    function logout(){
+    function logout() {
         Session::forget('student_session_key');
         Auth::guard('student')->logout();
         return redirect('/student_login');
     }
 
-    function forgetPassword(){
+    function forgetPassword() {
         return view('user_student.forget_password');
     }
-    
-    function resetPassword(Request $request){
+
+    function resetPassword(Request $request) {
         $request->validate([
-            'email'=>'required|email|exists:students,email'
+            'email' => 'required|email|exists:students,email'
         ]);
 
         $student = Student::where('email', $request->email)->first();
-        $password = Str::random(8); 
+        $password = Str::random(8);
         $password_hash = Hash::make($password);
         $student->password = $password_hash;
         $student->save();
 
         Mail::to($student->email)->send(new StudentForgetPassword($password));
 
-        if($student){
+        if ($student) {
             return redirect('/student_login')->with('success', 'Password Reseted successfully, Check Email');
-        }else{
+        } else {
             return redirect('/student_login')->with('error', 'Error Reseting Password');
         }
-
-
-        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\ClassroomApiController;
+use App\Http\Controllers\StudentAuthManager;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/student_login_api', [StudentAuthManager::class, 'loginPostApi']);
 
-Route::get('classrooms/{student_id}', [ClassroomApiController::class, 'index'] );
+
+Route::get('classrooms/{student_id}', [ClassroomApiController::class, 'index']);
