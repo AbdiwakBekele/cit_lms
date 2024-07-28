dab6df06b75a7b2ac62d5681ab95feb03701c492
abdiwak_bekele <abdiwakbek3226@gmail.com>
2024-07-29 02:45:25 +0300

    updated

--- resources/views/admin/student_managment/adminViewStudent.blade.php
+++ resources/views/admin/student_managment/adminViewStudent.blade.php
@@ -380,10 +380,130 @@
                 <strong>Enrolling Course</strong>
 
                 @foreach($student->classrooms as $classroom)
-                <div class="alert alert-light">
-                    <strong>{{$classroom->course->course_name}} </strong>
-                    <span> - {{$classroom->batch->batch_name}} shift</span>
-                    <span> - {{$classroom->batch->shift}} shift</span>
+                <div class="alert alert-light row">
+                    <div class="col-10">
+                        <strong>{{$classroom->course->course_name}} </strong>
+                        <span> - {{$classroom->batch->batch_name}} shift</span>
+                        <span> - {{$classroom->batch->shift}} shift</span>
+                    </div>
+                    <div class="col">
+                        <a href="#" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#report_{{$classroom->id}}">Report </a>
+
+                        <!-- Report Modal -->
+                        <div class="modal" id="report_{{$classroom->id}}">
+                            <div class="modal-dialog" style="max-width: 60%">
+                                <div class="modal-content">
+
+                                    <!-- Modal body -->
+                                    <div id="print_report" class="modal-body">
+
+                                        <!-- Header -->
+                                        <div class="m-3">
+                                            <h4
+                                                style="text-align: center; font-family: 'Times New Roman', Times, serif;">
+                                                <strong>
+                                                    CALIFORNIA TRAINING INSTITUTE
+                                                </strong></h3>
+                                                <h5
+                                                    style="text-align: center; font-family: 'Times New Roman', Times, serif;">
+                                                    <strong>
+                                                        Institutional and Cooperative Training
+                                                        Assessment
+                                                    </strong>
+                                            </h4>
+                                        </div>
+
+                                        <!-- Course | Batch | Student Info -->
+                                        <div class="m-4">
+                                            <p> <strong>Date:</strong> 09/06/2024</p>
+                                            <p> <strong>Unit of Competency:</strong> {{$classroom->course->course_name}}
+                                            </p>
+                                            <p> <strong>Module Code:</strong> {{$classroom->course->short_name}}</p>
+                                            <p> <strong>Batch Number:</strong> BT/{{$classroom->batch->batch_name}}/23
+                                            </p>
+                                            <p> <strong>Total Hour:</strong> 120</p>
+
+                                            <p><strong>Student Name: </strong> {{$student->fullname}}</p>
+                                        </div>
+
+                                        <!-- Score Table -->
+                                        <div>
+                                            <table class="table table-stripe m-4 ">
+                                                <tr>
+                                                    <th>Sessions</th>
+                                                    <th>L01</th>
+                                                    <th>L02</th>
+                                                    <th>L03</th>
+                                                    <th>L04</th>
+                                                    <th>Average</th>
+                                                </tr>
+
+                                                <tr>
+                                                    <td><strong>Theory (30%)</strong></td>
+                                                    <td>24.3</td>
+                                                    <td>28.7</td>
+                                                    <td>25.1</td>
+                                                    <td>26.4</td>
+                                                    <td>25.7</td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td><strong>Practice (30%)</strong></td>
+                                                    <td>33.1</td>
+                                                    <td>37.4</td>
+                                                    <td>35.1</td>
+                                                    <td>39.5</td>
+                                                    <td>36.7</td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td><strong>Cooperative Training (30%)</strong></td>
+                                                    <td>27</td>
+                                                    <td>26</td>
+                                                    <td>25</td>
+                                                    <td>27</td>
+                                                    <td>26.3</td>
+                                                </tr>
+                                            </table>
+
+                                            <table class="table table-stripe w-25" style="float: right;">
+                                                <tr>
+                                                    <th>Total</th>
+                                                    <td>88.7</td>
+                                                </tr>
+
+                                                <tr>
+                                                    <th>Grade</th>
+                                                    <td>B+</td>
+                                                </tr>
+                                            </table>
+                                        </div>
+
+
+                                        <!-- Signature -->
+                                        <div class="my-5" style="clear: both;">
+                                            <hr>
+                                            <p> <strong>Name of Instructor: __________________________</strong></p>
+                                            <br>
+                                            <p><strong>Signature: ____________________________________</strong> </p>
+                                        </div>
+
+
+                                    </div>
+
+                                    <!-- Modal footer -->
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-primary"
+                                            onclick="printModal()">Print</button>
+                                        <button type="button" class="btn btn-danger"
+                                            data-bs-dismiss="modal">Close</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
 
@@ -426,6 +546,18 @@ function fetchBatches(courseId) {
                 batchSelect.appendChild(option);
             });
         });
+}
+
+// Print Transcript
+function printModal() {
+    var printContents = document.querySelector('#print_report').innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+
+    location.reload(); // To reload the page and restore the original contents
 }
 </script>
 
