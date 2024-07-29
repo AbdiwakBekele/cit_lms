4dbdac2100648c5201cd9ac5f1b6921a11b4eb64
abdiwak_bekele <abdiwakbek3226@gmail.com>
2024-07-29 08:40:00 +0300

    updated

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -20,7 +20,9 @@ class FeedbackController extends Controller {
      * Show the form for creating a new resource.
      */
     public function create() {
-        return view('feedback.feedback_student');
+        // return view('feedback.feedback_student');
+        $feedbacks = [];
+        return view('admin.feedback.feedback_index', compact('feedbacks'));
     }
 
     /**
